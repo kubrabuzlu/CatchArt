87237f906c1354fafba6d662de5e7213cb147aca
kubrabuzlu <kubraa.buzlu@gmail.com>
2025-05-07 17:20:02 +0300

    chore:minor updates

--- src/utils.py
+++ src/utils.py
@@ -3,26 +3,22 @@ from pathlib import Path
 from typing import Union
 import os
 
+
 def save_model(model: torch.nn.Module,
-               target_dir: Union[str, Path],
-               model_name: str) -> None:
+               target_dir: Union[str, Path]) -> None:
     """
     Saves a PyTorch model's state_dict to a specified directory.
 
     Args:
         model (torch.nn.Module): The PyTorch model to be saved.
         target_dir (str or Path): Directory path to save the model.
-        model_name (str): Name of the model file (must end with .pth or .pt).
 
     Raises:
         AssertionError: If the model_name does not end with '.pt' or '.pth'.
         OSError: If unable to write to the target directory.
-
-    Example:
-        save_model(model, "models", "resnet50_best.pth")
     """
     # Ensure valid extension
-    assert model_name.endswith(".pth") or model_name.endswith(".pt"), \
+    assert target_dir.endswith(".pth") or target_dir.endswith(".pt"), \
         "model_name must end with '.pt' or '.pth'"
 
     # Ensure directory exists
@@ -32,9 +28,6 @@ def save_model(model: torch.nn.Module,
     except OSError as e:
         raise OSError(f"Failed to create directory {target_dir}") from e
 
-    # Full model path
-    model_path = target_dir / model_name
-
     # Save state_dict
-    torch.save(model.state_dict(), model_path)
-    print(f"[INFO] Model saved to {model_path.resolve()}")
+    torch.save(model.state_dict(), target_dir)
+    print(f"[INFO] Model saved to {target_dir.resolve()}")
