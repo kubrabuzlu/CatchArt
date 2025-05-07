ef4260f3255e8f8037a70ae928b991ae0b016611
kubrabuzlu <kubraa.buzlu@gmail.com>
2025-05-07 17:19:21 +0300

    feat(training): enhance training loop with early stopping, LR scheduler, and wandb logging
    - Added early stopping based on minimum validation loss to prevent overfitting and reduce unnecessary training
    - Integrated ReduceLROnPlateau scheduler to dynamically reduce learning rate when test loss plateaus
    - Logged key training metrics (loss, accuracy, F1) and learning rate to Weights & Biases (wandb) for experiment tracking
    - Saved best model checkpoint based on test loss
    
    These improvements help stabilize training, enable better generalization, and facilitate experiment monitoring.

--- src/engine.py
+++ src/engine.py
@@ -1,13 +1,17 @@
+import wandb
 import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn.metrics import f1_score
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
+from utils import save_model
 
 
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
                optimizer: torch.optim.Optimizer,
-               device: torch.device) -> Tuple[float, float]:
+               device: torch.device) -> Tuple[float, float, float]:
     """
     Performs a single training step over the entire dataloader.
 
@@ -22,9 +26,10 @@ def train_step(model: torch.nn.Module,
         Tuple[float, float]: Average training loss and accuracy for the epoch.
     """
     model.train()
-    train_loss, train_acc = 0, 0
+    total_loss, total_acc = 0, 0
+    all_preds, all_labels = [], []
 
-    for batch, (X, y) in enumerate(dataloader):
+    for X, y in dataloader:
         X, y = X.to(device), y.to(device)
 
         # Forward pass
@@ -32,26 +37,33 @@ def train_step(model: torch.nn.Module,
 
         # Compute loss
         loss = loss_fn(y_pred, y)
-        train_loss += loss.item()
+        total_loss += loss.item()
 
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
+
+        # Optimizer
         optimizer.step()
 
         # Compute accuracy
-        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
+        preds = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        total_acc += (preds == y).sum().item() / len(y_pred)
 
-    train_loss = train_loss / len(dataloader)
-    train_acc = train_acc / len(dataloader)
-    return train_loss, train_acc
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(y.cpu().numpy())
+
+    avg_loss = total_loss / len(dataloader)
+    avg_acc = total_acc / len(dataloader)
+    f1 = f1_score(all_labels, all_preds, average='weighted')
+
+    return avg_loss, avg_acc, f1
 
 
 def test_step(model: torch.nn.Module,
               dataloader: torch.utils.data.DataLoader,
               loss_fn: torch.nn.Module,
-              device: torch.device) -> Tuple[float, float]:
+              device: torch.device) -> Tuple[float, float, float]:
     """
     Evaluates the model on a given test/validation dataset.
 
@@ -65,26 +77,31 @@ def test_step(model: torch.nn.Module,
         Tuple[float, float]: Average test loss and accuracy.
     """
     model.eval()
-    test_loss, test_acc = 0, 0
+    total_loss, total_acc = 0, 0
+    all_preds, all_labels = [], []
 
     with torch.inference_mode():
-        for batch, (X, y) in enumerate(dataloader):
+        for X, y in dataloader:
             X, y = X.to(device), y.to(device)
 
             # Forward pass
-            test_pred_logits = model(X)
+            y_pred = model(X)
 
             # Compute loss
-            loss = loss_fn(test_pred_logits, y)
-            test_loss += loss.item()
+            loss = loss_fn(y_pred, y)
+            total_loss += loss.item()
 
             # Compute accuracy
-            test_pred_labels = test_pred_logits.argmax(dim=1)
-            test_acc += (test_pred_labels == y).sum().item() / len(test_pred_labels)
+            preds = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+            total_acc += (preds == y).sum().item() / len(y)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(y.cpu().numpy())
 
-    test_loss = test_loss / len(dataloader)
-    test_acc = test_acc / len(dataloader)
-    return test_loss, test_acc
+    avg_loss = total_loss / len(dataloader)
+    avg_acc = total_acc / len(dataloader)
+    f1 = f1_score(all_labels, all_preds, average='weighted')
+
+    return avg_loss, avg_acc, f1
 
 
 def train(model: torch.nn.Module,
@@ -93,7 +110,11 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          device: torch.device) -> Dict[str, List]:
+          device: torch.device,
+          checkpoint_path: str,
+          log_wandb: bool=False,
+          early_stopping_patience: int=5,
+          scheduler_patience: int=3) -> Dict[str, List]:
     """
     Trains and evaluates the model over multiple epochs.
 
@@ -105,38 +126,87 @@ def train(model: torch.nn.Module,
         loss_fn (torch.nn.Module): Loss function to calculate model error.
         epochs (int): Number of training epochs.
         device (torch.device): Device to train the model on (CPU or CUDA).
+        checkpoint_path (str): File path to save the best model.
+        log_wandb (bool): Specifies whether Wandb will be used.
+        early_stopping_patience (int): Number of steps to wait for early stopping.
+        scheduler_patience (int): Number of steps to wait for reduce learning rate.
 
     Returns:
         Dict[str, List]: A dictionary containing lists of loss and accuracy for both training and test sets.
     """
+
+    best_loss = 0
+    patience_counter = 0
     results = {"train_loss": [],
                "train_acc": [],
+               "train_f1": [],
                "test_loss": [],
-               "test_acc": []}
+               "test_acc": [],
+               "test_f1": []}
+
+    scheduler = ReduceLROnPlateau(
+        optimizer,
+        mode='min',
+        factor=0.5,
+        patience=scheduler_patience,
+        verbose=True
+    )
 
     for epoch in tqdm(range(epochs)):
-        train_loss, train_acc = train_step(model=model,
-                                           dataloader=train_dataloader,
-                                           loss_fn=loss_fn,
-                                           optimizer=optimizer,
-                                           device=device)
-        test_loss, test_acc = test_step(model=model,
-                                        dataloader=test_dataloader,
-                                        loss_fn=loss_fn,
-                                        device=device)
+        train_loss, train_acc, train_f1 = train_step(model=model,
+                                                     dataloader=train_dataloader,
+                                                     loss_fn=loss_fn,
+                                                     optimizer=optimizer,
+                                                     device=device)
+        test_loss, test_acc, test_f1 = test_step(model=model,
+                                                 dataloader=test_dataloader,
+                                                 loss_fn=loss_fn,
+                                                 device=device)
 
         print(
             f"Epoch: {epoch + 1} | "
             f"train_loss: {train_loss:.4f} | "
             f"train_acc: {train_acc:.4f} | "
+            f"train_f1: {train_f1:.4f} | "
             f"test_loss: {test_loss:.4f} | "
-            f"test_acc: {test_acc:.4f}"
+            f"test_acc: {test_acc:.4f} | "
+            f"test_f1: {test_f1:.4f}"
         )
+
+        if log_wandb:
+            wandb.log({
+                "epoch": epoch + 1,
+                "learning_rate": optimizer.param_groups[0]['lr'],
+                "train_loss": train_loss,
+                "train_acc": train_acc,
+                "train_f1": train_f1,
+                "test_loss": test_loss,
+                "test_acc": test_acc,
+                "test_f1": test_f1,
+            })
+
+        # Save best model
+        if test_loss < best_loss:
+            best_loss = test_loss
+            patience_counter = 0
+            save_model(model.state_dict(), checkpoint_path)
+            print(f"Best model saved at epoch {epoch + 1} with loss: {best_loss:.4f}")
+        else:
+            patience_counter += 1
+            print(f"No improvement. Patience: {patience_counter}/{early_stopping_patience}")
+
+        # Early stopping
+        if patience_counter >= early_stopping_patience:
+            print(f"Early stopping at epoch {epoch + 1}")
+            break
 
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
+        results["train_f1"].append(train_f1)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
+        results["test_f1"].append(test_f1)
+
+        scheduler.step(test_loss)
 
     return results
-
